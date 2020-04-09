7daaa171b5a9d83d7eac60cce3569dad3d510eb3
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-09 17:26:24 -0400

    data entry logging for all text fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,10 +5,7 @@ class Form extends Component {
         super(props);
 
         this.state = {
-            name: '',
-            password: '',
-            age: '',
-            gender: ''
+            value: ''
         }
 
         /*    this.typingChange = this.typingChange.bind;
@@ -17,12 +14,14 @@ class Form extends Component {
 
     typingChange = (event) => {
         this.setState({
-            name: event.target.value
+            value: event.target.value
         })
     }
 
     handleSubmit = (event) => {
         /* alert(' A name was submitted: ' + this.state.value) */
+        let el = document.createElement("div");
+        el.innerHTML = '<p>{this.state.value}</p>'
         console.log(this.state.value)
         event.preventDefault()
     }
@@ -33,18 +32,8 @@ class Form extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <label> Username: </label>
                     <input type="text" value={this.state.value} onChange={this.typingChange} name="name" />
-                    <label> Password: </label>
-                    <input type="text" value={this.state.value} onChange={this.typingChange} password="password" />
-                    <label> Age: </label>
-                    <input type="text" value={this.state.value} onChange={this.typingChange} age="age" />
-                    <label> Gender: </label>
-                    <input type="text" value={this.state.value} onChange={this.typingChange} gender="gender" />
                     <input type="submit" value="Submit" />
                 </form>
-                <h1>{this.state.name}</h1>
-                <h1>{this.state.password}</h1>
-                <h1>{this.state.age}</h1>
-                <h1>{this.state.gender}</h1>
             </div>
         );
     }
